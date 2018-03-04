37c84cebef352f8783bc4c19156933267837acc2
chris <orthagonal1@gmail.com>
2018-03-04 13:15:38 -0600

    basic test up

--- tests/setup.js
+++ tests/setup.js
@@ -1,8 +1,26 @@
 const Rapptor = require('rapptor');
 
+//todo: add mock pagedata host
+
 // todo: in future this will be changed to just use async/await
 // everything else in the tests should be fine
 module.exports.setup = (options, dataSet) => new Promise((resolve, reject) => {
+  const testenv = {
+    API_KEY: 'none',
+    ELASTICSEARCH_HOST: 'http://localhost:9200',
+    ELASTICSEARCH_INDEX: 'test' ,
+    ELASTICSEARCH_TYPE: 'string' ,
+    PAGEDATA_HOST: 'localhost:9000',
+    PAGEDATA_KEY: 'test' ,
+    PAGEDATA_SEARCH_OBJECT: '' ,
+    PAGEDATA_STATUS: 'draft' ,
+  };
+
+  const allEnv = Object.assign({}, testenv, options.testenv);
+  Object.keys(allEnv).forEach(key => {
+    process.env[key] = allEnv[key];
+  });
+
   const rapptor = new Rapptor({});
   rapptor.start((err, result) => {
     if (err) {
